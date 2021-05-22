63d1d1407959777a82cc3247cded32a293e7cf0f
redpandaathome <leeyumi415@gmail.com>
2021-05-22 11:52:09 +0900

    basic echo

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "github.com/redpandaathome/learngo/scrapper"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 // go get github.com/labstack/echo/v4
 
+// Handler
+func handleHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
 func main() {
-	scrapper.Scrape("golang")
+	// scrapper.Scrape("golang")
+	e := echo.New()
+
+	// Routes
+	e.GET("/", handleHome)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
